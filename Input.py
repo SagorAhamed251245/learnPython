ac4ac15555fadde47cf4e2da527f8cf933ed954e
Sagor Ahamed <104268039+SagorAhamed251245@users.noreply.github.com>
2024-03-01 00:28:26 +0600

    tinput

---
+++ Input.py
@@ -0,0 +1,12 @@
+""" a = input("Type your name: ")
+
+print("Your name is:",a) 
+ """
+firstNumber = input("Enter First Number: ") 
+secundNumber = input("Enter Secund Number: ") 
+
+x = int(firstNumber)
+y = int(secundNumber)
+
+
+print(x + y)
